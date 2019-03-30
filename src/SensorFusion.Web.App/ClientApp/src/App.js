18ff2bdd8ec0ea41d94eebff2ade117c832ce1b8
Andrew Rublyov <andrewrublyov99@gmail.com>
2019-03-30 18:22:47 +0200

    UI progress

--- src/SensorFusion.Web.App/ClientApp/src/App.js
+++ src/SensorFusion.Web.App/ClientApp/src/App.js
@@ -1,20 +1,26 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router';
-import { Layout } from './components/Layout';
+import Layout from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+
+const theme = createMuiTheme();
 
 export default class App extends Component {
   static displayName = App.name;
 
   render () {
     return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-      </Layout>
+      <MuiThemeProvider theme={theme}>
+        <Layout>
+          <Route exact path='/' component={Home} />
+          <Route path='/counter' component={Counter} />
+          <Route path='/fetch-data' component={FetchData} />
+        </Layout>
+      </MuiThemeProvider>
     );
   }
 }
--- src/SensorFusion.Web.App/ClientApp/src/components/Layout.js
+++ src/SensorFusion.Web.App/ClientApp/src/components/Layout.js
@@ -1,18 +1,197 @@
-import React, { Component } from 'react';
-import { Container } from 'reactstrap';
-import { NavMenu } from './NavMenu';
+import React, {Component} from 'react';
+import {Container} from 'reactstrap';
+import CssBaseline from "@material-ui/core/CssBaseline";
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import {withStyles} from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import Badge from '@material-ui/core/Badge';
 
-export class Layout extends Component {
+import List from '@material-ui/core/List';
+import ListItem from "@material-ui/core/ListItem";
+
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import DashboardIcon from "@material-ui/core/SvgIcon/SvgIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListSubheader from "@material-ui/core/ListSubheader";
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import PeopleIcon from '@material-ui/icons/People';
+import BarChartIcon from '@material-ui/icons/BarChart';
+import LayersIcon from '@material-ui/icons/Layers';
+import AssignmentIcon from '@material-ui/icons/Assignment';
+
+const drawerWidth = 240;
+
+const styles = theme => ({
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  drawerPaper: {
+    position: 'relative',
+    whiteSpace: 'nowrap',
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerPaperClose: {
+    overflowX: 'hidden',
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    width: theme.spacing.unit * 7,
+    [theme.breakpoints.up('sm')]: {
+      width: theme.spacing.unit * 9,
+    },
+  },
+  appBarSpacer: theme.mixins.toolbar,
+});
+
+class Layout extends Component {
   static displayName = Layout.name;
 
-  render () {
+  state = {
+    open: true,
+  };
+
+  handleDrawerOpen = () => {
+    this.setState({open: true});
+  };
+
+  handleDrawerClose = () => {
+    this.setState({open: false});
+  };
+
+  render() {
     return (
       <div>
-        <NavMenu />
-        <Container>
-          {this.props.children}
-        </Container>
+        <CssBaseline/>
+        <AppBar
+          position="static"
+          className={classNames(this.props.classes.appBar, this.state.open && this.props.classes.appBarShift)}
+        >
+          <Toolbar disableGutters={!this.state.open} className={this.props.classes.toolbar}>
+            <IconButton
+              color="inherit"
+              aria-label="Open drawer"
+              onClick={this.handleDrawerOpen}
+              className={classNames(
+                this.props.classes.menuButton,
+                this.state.open && this.props.classes.menuButtonHidden,
+              )}
+            >
+              <MenuIcon/>
+            </IconButton>
+            <Typography
+              component="h1"
+              variant="h6"
+              color="inherit"
+              noWrap
+              className={this.props.classes.title}
+            >
+              Dashboard
+            </Typography>
+            <IconButton color="inherit">
+              <Badge badgeContent={4} color="secondary">
+                <NotificationsIcon/>
+              </Badge>
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          variant="permanent"
+          classes={{
+            paper: classNames(this.props.classes.drawerPaper, !this.state.open && this.props.classes.drawerPaperClose),
+          }}
+          open={this.state.open}
+        >
+          <div className={this.props.classes.toolbarIcon}>
+            <IconButton onClick={this.handleDrawerClose}>
+              <ChevronLeftIcon/>
+            </IconButton>
+          </div>
+          <Divider/>
+          <List>
+            <ListItem button>
+              <ListItemIcon>
+                <DashboardIcon/>
+              </ListItemIcon>
+              <ListItemText primary="Dashboard"/>
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+                <ShoppingCartIcon/>
+              </ListItemIcon>
+              <ListItemText primary="Orders"/>
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+                <PeopleIcon/>
+              </ListItemIcon>
+              <ListItemText primary="Customers"/>
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+                <BarChartIcon/>
+              </ListItemIcon>
+              <ListItemText primary="Reports"/>
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+                <LayersIcon/>
+              </ListItemIcon>
+              <ListItemText primary="Integrations"/>
+            </ListItem>
+          </List>
+          <Divider/>
+          <List>
+            <ListSubheader inset>Saved reports</ListSubheader>
+            <ListItem button>
+              <ListItemIcon>
+                <AssignmentIcon />
+              </ListItemIcon>
+              <ListItemText primary="Current month" />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+                <AssignmentIcon />
+              </ListItemIcon>
+              <ListItemText primary="Last quarter" />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+                <AssignmentIcon />
+              </ListItemIcon>
+              <ListItemText primary="Year-end sale" />
+            </ListItem>
+          </List>
+        </Drawer>
+        <main className={this.props.classes.content}>
+          <Container>
+            {this.props.children}
+          </Container>
+        </main>
       </div>
     );
   }
 }
+
+Layout.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Layout);
